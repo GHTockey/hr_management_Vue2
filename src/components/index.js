371dc10b44a2c2f8bdb3f8e1ba7dd591414ae455
Tockey <tockey@yeah.net>
2022-11-09 11:02:51 +0800

    完成：tab页的视图引入

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
 import PageTools from "./PageTools";
+import TagsView from "./TagsView";
 export default {
     install(Vue) {
-        Vue.component('PageTools', PageTools)
+        Vue.component('PageTools', PageTools),
+        Vue.component('TagsView', TagsView)
     }
 }
